adf6b630028a7251905c01d547ef5143c2b843fa
mainmas2964 <nonameofmnms123@gmail.com>
2025-10-04 20:35:27 +0500

    test

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
 int main(int argc, char** argv) {
+    //test
     std::cout << "FractalHorizon3DEngine starting..." << std::endl;
 
     Engine engine;
