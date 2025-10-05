d536cc5aae07871e193682e8378f33e3ad865a65
mainmas2964 <nonameofmnms123@gmail.com>
2025-10-05 13:11:04 +0500

    new changes

--- src/ECS/component/component.cpp
+++ src/ECS/component/component.cpp
@@ -3,44 +3,56 @@ created by mainmas2964  2025, 5 octomber
 */
 #include "component.h"
 #include <stdexcept>
-#include <iostream>
-#include <vector>
-#include "ECS/entity/entity.h"
 #include <cstdint>
-
+#include <limits>
+#include <utility> // std::move
+#include "ECS/entity/entity.h"
 template<typename T>
 void Component<T>::addComponent(Entity e, const T& component) {
-    if (e >= sparse.size()) {
-        throw std::out_of_range("Entity id exceeds maximum limit");
+    uint32_t entity_id = e.id;
+    assert(entity_id < sparse.size() && "Entity ID exceeds maximum entities");
+    if (sparse[entity_id] != 0xFFFFFFFF) {
+        throw std::runtime_error("Entity already has this component");
     }
-    if (sparse[e] != 0xFFFFFFFF) {
-        throw std::runtime_error("Entity already has component");
-    }
-    sparse[e] = comp.size();
-    dense.push_back(e);
+    sparse[entity_id] = static_cast<uint32_t>(comp.size());
+    dense.push_back(entity_id);
     comp.push_back(component);
 }
 template<typename T>
 void Component<T>::removeComponent(Entity e) {
-    if (e >= sparse.size() || sparse[Entity] == 0xFFFFFFFF)
-        throw std::runtime_error("Entity does not have component");
-    uint32_t index = sparse[e]; 
-    uint32_t lastEntity = dense.back();
-    comp[index] = comp.back();
-    dense[index] = lastEntity;
-    sparse[lastEntity] = index;
+    uint32_t entity_id = e.id;
+    assert(entity_id < sparse.size() && "Entity ID exceeds maximum entities");
+    uint32_t index = sparse[entity_id];
+    if (index == 0xFFFFFFFF) {
+        throw std::runtime_error("Entity does not have this component");
+    }
+    uint32_t last_entity_id = dense.back();
+    comp[index] = std::move(comp.back());
+    dense[index] = last_entity_id;
+    sparse[last_entity_id] = index;
     comp.pop_back();
     dense.pop_back();
-    sparse[e] = 0xFFFFFFFF;
+    sparse[entity_id] = 0xFFFFFFFF; // mark as removed
 }
 template<typename T>
 bool Component<T>::hasComponent(Entity e) {
-    if (e >= sparse.size()) return false;
-    return sparse[e] != 0xFFFFFFFF;
+    uint32_t entity_id = e.id;
+    assert(entity_id < sparse.size() && "Entity ID exceeds maximum entities");
+    return sparse[entity_id] != 0xFFFFFFFF;
 }
 template<typename T>
 T& Component<T>::getComponent(Entity e) {
-    if (Entity >= sparse.size() || sparse[entity] == 0xFFFFFFFF)
-        throw std::runtime_error("Entity does not have component");
-    return comp[sparse[e]];
+    uint32_t entity_id = e.id;
+    assert(entity_id < sparse.size() && "Entity ID exceeds maximum entities");
+    uint32_t index = sparse[entity_id];
+    if (index == 0xFFFFFFFF) {
+        throw std::runtime_error("Entity does not have this component");
+    }
+    return comp[index];
 }
+// Explicit template instantiation for common component types
+template class Component<int>;
+template class Component<float>;
+template class Component<double>;
+template class Component<std::string>;
+// Add more types as needed
