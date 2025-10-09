0ea462da7f09069d46741f729defd7739ffeaa3c
mainmas2964 <nonameofmnms123@gmail.com>
2025-10-09 10:05:49 +0500

    test rotation

--- src/render/UniformState.cpp
+++ src/render/UniformState.cpp
@@ -1,13 +1,17 @@
 #include "UniformState.h"
-
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 void UniformState::initDefault() {
     resolution = glm::vec2(1280.0f, 720.0f);
     radius = 0.4f;
     color = glm::vec3(1.0f, 0.2f, 0.1f);
+    mat4 = glm::mat4(1.0f);
 }
 
-void UniformState::upload(const Shader& shader) const {
+void UniformState::upload(const Shader& shader) {
     shader.setVec2("u_resolution", resolution);
     shader.setFloat("u_radius", radius);
     shader.setVec3("u_color", color);
+    shader.setMat4("u_trans", mat4);
+    mat4 = glm::rotate(mat4, glm::radians(0.5f), glm::vec3(0.5f, 0.5f, 0.5f));   
 }
--- src/render/UniformState.h
+++ src/render/UniformState.h
@@ -2,12 +2,14 @@
 #include <glm/vec2.hpp>
 #include <glm/vec3.hpp>
 #include "Shader.h"
-
+#include <glm/mat4x4.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 struct UniformState {
     glm::vec2 resolution;
     float radius;
     glm::vec3 color;
+    glm::mat4 mat4; 
 
     void initDefault();
-    void upload(const Shader& shader) const; // передает автоматически да я ленивый
+    void upload(const Shader& shader); // передает автоматически да я ленивый
 };
