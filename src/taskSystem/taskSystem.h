d5fd65b45d400ff5fc917db223ef901cf4a3d25f
mainmas2964 <nonameofmnms123@gmail.com>
2025-12-03 16:38:16 +0300

    something

---
+++ src/taskSystem/taskSystem.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+#include <functional>
+#include <atomic>
+struct Task{
+    std::function<void()> func;
+}
